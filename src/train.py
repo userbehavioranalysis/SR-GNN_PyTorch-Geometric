73a83b31b5a15339087be965c36d6e78e9a761ed
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-05-31 10:08:08 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -9,7 +9,7 @@ import numpy as np
 import logging
 
 
-def forward(model, loader, device, writer, epoch, top_k=20, optimizer=None, train_flag=True):
+def forward(para, model, loader, device, writer, epoch, top_k=20, optimizer=None, train_flag=True):
     if train_flag:
         model.train()
     else:
@@ -26,6 +26,8 @@ def forward(model, loader, device, writer, epoch, top_k=20, optimizer=None, trai
         targets = batch.y - 1
         loss = model.loss_function(scores, targets)
 
+        if para.auxiliary_nodes:
+            loss += model.loss_nodes()
         if train_flag:
             loss.backward()
             optimizer.step()
